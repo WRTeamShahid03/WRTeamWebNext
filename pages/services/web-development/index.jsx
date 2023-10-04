1f8a79090eab1ee9c0f577d01beba0e595b94201
Sakilluhar <sakilluharpc@gmail.com>
2023-10-04 18:47:52 +0530

    4-10 6:48

--- pages/services/web-development/index.jsx
+++ pages/services/web-development/index.jsx
@@ -1,8 +1,67 @@
 import Breadcrum from '@/Components/Breadcrum'
 import React from 'react'
+import circle from '../../../src/Asset/Images/Shape_1.svg'
+import dottsShape from '../../../src/Asset/Images/Shape_2.png'
 import crossShape from '../../../src/Asset/Images/Shape_3.png'
+import triangle from '../../../src/Asset/Images/Shape_4.svg'
+import Card from 'react-bootstrap/Card';
 
 const index = () => {
+
+  const benefitsCardData = [
+    {
+      id: 0,
+      icon: '',
+      title: 'Timely Updates'
+
+    },
+    {
+      id: 1,
+      icon: '',
+      title: 'Testing & Quality Assurance'
+
+    },
+    {
+      id: 2,
+      icon: '',
+      title: 'Ongoing Support'
+
+    },
+    {
+      id: 3,
+      icon: '',
+      title: 'Elite Author on Codecanyon'
+
+    },
+  ]
+
+  const processCardData = [
+    {
+      id: 0,
+      icon: '',
+      title: 'Timely Updates'
+
+    },
+    {
+      id: 1,
+      icon: '',
+      title: 'Testing & Quality Assurance'
+
+    },
+    {
+      id: 2,
+      icon: '',
+      title: 'Ongoing Support'
+
+    },
+    {
+      id: 3,
+      icon: '',
+      title: 'Elite Author on Codecanyon'
+
+    },
+  ]
+
   return (
     <>
       <Breadcrum title='Web' blueText={'Development'} contentOne={'Home'} contentTwo={'Services'} contentThree={'Web Development'} />
@@ -31,6 +90,7 @@ const index = () => {
 
         </div>
       </section>
+      {/* development ends */}
 
       <section id='backend'>
         <div className="container">
@@ -52,14 +112,19 @@ const index = () => {
                 <div className="col-sm-12 col-md-12 col-lg-6 contentWrapper">
 
                   <div className="leftDivContent">
-                    <div className="shapesDiv">
-                      <img src={crossShape.src} alt="" />
+                    <div className="leftDivshapes">
+                      <img src={crossShape.src} alt="" className='crossShape' />
+                      <img src={dottsShape.src} alt="" className='dottsShape' />
                     </div>
                   </div>
 
                 </div>
                 <div className="col-sm-12 col-md-12 col-lg-6 contentWrapper">
                   <div className="rightDivContent">
+                    <div className="rightDivshapes">
+                      <img src={circle.src} alt="" className='circleShape' />
+                      <img src={triangle.src} alt="" className='triangleShape' />
+                    </div>
                     <span>Back-end web development</span>
                     <p className='comman_para firstP'>Our backend web development experts focus to developing powerful, scalable server-side applications which support website functionality. We use various programming languages such as JavaScript, PHP, Ruby and frameworks such as Django, Laravel and Ruby on Rails to build secure and efficient backend solutions for your business requirements.</p>
                   </div>
@@ -72,6 +137,92 @@ const index = () => {
           </div>
         </div>
 
+      </section>
+      {/* backend ends  */}
+
+      <section id='benefits' className='container'>
+        <div className="row">
+          <div className="col-sm col-md-12 col-lg-6">
+            <div className="benefitsHeadings">
+              <span className="common_span">
+                Extensive <span>Benefits</span>
+              </span>
+              <h3 className='comman_Headlines'>Make a Website With the Best <span>Web Development</span> Company</h3>
+            </div>
+          </div>
+
+          <div className="col-sm col-md-12 col-lg-6">
+            <div className="benefitsPara">
+              <p className='comman_para'>Our web development service is ready to meet your unique business needs and deliver high-quality, responsive websites that are visually appealing and fully functional.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="row benefitsCards">
+          {
+            benefitsCardData.map((e) => {
+              return <div className="col-sm-12 col-md-4 col-lg-3" key={e.id}>
+                <Card>
+                  {/* <Card.Img variant="top" src="..." /> */}
+                  <span className='card-img-top'></span>
+                  <Card.Body>
+                    <Card.Title>{e.title}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </div>
+            })
+          }
+        </div>
+      </section>
+      {/* benefits ends  */}
+
+      <section id='process' className='container'>
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-6">
+              <div className="upperDivLeftContent">
+                <img src="..." alt="" />
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-6">
+            <div className="upperDivRightContent">
+              <div className="upperRightHeadings">
+                <span className="common_span">
+                  Our <span>Work Process</span>
+                </span>
+                <h3 className='comman_Headlines' data-aos="fade-right" data-aos-duration="1000">
+                  Make a Website With the Best <span>Web Development</span> Company
+                </h3>
+              </div>
+
+              <div className="processCardWrapper">
+
+              <div className="processCard">
+                <span className='cardIcon'></span>
+                <span className='cardContent'>
+                  <span className='title'>01.Idea</span>
+                  <span className='comman_para'>Our web development service is ready to meet your unique business needs and deliver high-quality.</span>
+                </span>
+              </div>
+              <div className="processCard">
+                <span className='cardIcon'></span>
+                <span className='cardContent'>
+                  <span className='title'>01.Idea</span>
+                  <span className='comman_para'>Our web development service is ready to meet your unique business needs and deliver high-quality.</span>
+                </span>
+              </div>
+              <div className="processCard">
+                <span className='cardIcon'></span>
+                <span className='cardContent'>
+                  <span className='title'>01.Idea</span>
+                  <span className='comman_para'>Our web development service is ready to meet your unique business needs and deliver high-quality.</span>
+                </span>
+              </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
